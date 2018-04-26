52a89db5eb6266c10cc3f626667d6529b3f755ef
AthiyaG <athiyageorge253@gmail.com>
2018-04-26 16:22:13 +0530

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/all',function(req,res){
-	res.write("hi dears.... ");
-	res.end();
+	myObj = { "name":"John", "age":30, "car":"BMW" };
+	//res.write("hi dears.... ");
+	var JSONdata = JSON.stringify(myObj);
+    res.send(JSONdata);
+	//res.end();
 	
 })
 
